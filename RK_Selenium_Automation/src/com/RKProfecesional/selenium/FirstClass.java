9b6f752d90398abe4a155ea594f53b7093f3cf59
madhukb2006 <madhukb2006@gmail.com>
2018-10-17 22:59:03 +0530

    Added new project RK_Selenium_Automation
    Added new project RK_Selenium_Automation

---
+++ RK_Selenium_Automation/src/com/RKProfecesional/selenium/FirstClass.java
@@ -0,0 +1,10 @@
+package com.RKProfecesional.selenium;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is the first java class by Ravikanth");
+
+	}
+
+}
